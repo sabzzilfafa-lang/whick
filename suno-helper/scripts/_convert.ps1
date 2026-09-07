c9c7b2beacbf13ff822b3a99cc874d77c737a0bb
user <user@localhost>
2026-09-07 13:36:06 +0900

    suno-helper: account.html Korean mojibake fix (HTML entities + JS unicode escapes)

--- suno-helper/scripts/_convert.ps1
+++ suno-helper/scripts/_convert.ps1
@@ -1,3 +1,4 @@
 $rr = (Get-Content installer\web\check_served.sh -Raw) -replace "`r`n","`n"
-[System.IO.File]::WriteAllText("$env:TEMP\cs6.sh", $rr, (New-Object System.Text.UTF8Encoding($false)))
-cmd /c "scp -q %TEMP%\cs6.sh whick-server:/tmp/cs6.sh && ssh whick-server ""bash /tmp/cs6.sh; rm -f /tmp/cs6.sh"""
+[System.IO.File]::WriteAllText("$env:TEMP\cs7.sh", $rr, (New-Object System.Text.UTF8Encoding($false)))
+cmd /c "ssh whick-server ""bash /tmp/cs7.sh 2>/dev/null; rm -f /tmp/cs7.sh"""
+git ls-remote origin main
